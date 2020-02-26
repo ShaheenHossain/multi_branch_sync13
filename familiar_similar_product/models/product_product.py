fefd6a0d1f2bba856715164a53c815e9e2178166
Sam <sam@synconics.in>
2020-02-26 17:14:26 +0530

    [MOD] Code in familiar_similar_product module

--- familiar_similar_product/models/product_product.py
+++ familiar_similar_product/models/product_product.py
@@ -11,7 +11,7 @@ class ProductProduct(models.Model):
     def _name_search(self, name, args=None, operator='ilike', limit=100, name_get_uid=None):
         if self._context.get("filter_spare_parts", False):
             if self._context.get("ticket_product_id", False):
-                ticket_product = self.browse(self._context.getd("ticket_product_id"))
+                ticket_product = self.browse(self._context.get("ticket_product_id"))
                 args = expression.AND([
                     args or [],
                     [('id', 'in', ticket_product.spare_part_product_ids.ids)]
